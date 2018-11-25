38bf732df7b1efc56736c5088d8fe3817d0b3f73
Kelly Selden <kellyselden@gmail.com>
2018-11-25 16:23:56 +0000

    fix local error

--- src/get-package-version.js
+++ src/get-package-version.js
@@ -2,6 +2,7 @@
 
 const run = require('./run-async');
 const pMap = require('p-map');
+const pRetry = require('p-retry');
 
 module.exports = function getPackageVersion({
   dependencies,
@@ -20,17 +21,28 @@ module.exports = function getPackageVersion({
           return;
         }
 
-        return run(`npm info react-scripts@${reactScriptsVersion} dependencies --json`).then(results => {
-          if (packageVersion) {
-            return;
-          }
+        return pRetry(() => {
+          return run(`npm info react-scripts@${reactScriptsVersion} dependencies --json`).then(results => {
+            if (packageVersion) {
+              return;
+            }
 
-          let dependencies = JSON.parse(results);
+            let dependencies = JSON.parse(results);
 
-          if (dependencies['react-dev-utils'] === reactDevUtilsVersion) {
-            packageVersion = `^${reactScriptsVersion}`;
-          }
-        });
+            if (dependencies['react-dev-utils'] === reactDevUtilsVersion) {
+              packageVersion = `^${reactScriptsVersion}`;
+            }
+          }).catch(err => {
+            // occurs sometimes when running multiple npm calls at once
+            if (typeof err !== 'string' || !err.includes('npm update check failed')) {
+              throw new pRetry.AbortError(err);
+            }
+
+            // https://github.com/sindresorhus/p-retry/issues/14
+            // throw err;
+            throw new Error(err);
+          });
+        }, { retries: 5 });
       }, { concurrency: 5 });
     }
 
