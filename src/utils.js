be4b6f61b5244f7c792a4ba2577ffcbca16c8c1f
Kelly Selden <kellyselden@gmail.com>
2019-01-01 16:59:55 +0000

    move getApplicableCodemods

--- src/utils.js
+++ src/utils.js
@@ -4,9 +4,6 @@ const denodeify = require('denodeify');
 
 module.exports.run = require('./run');
 module.exports.npx = require('./npx');
-module.exports.getCodemods = require('boilerplate-update/src/get-codemods').bind(null, 'https://cdn.jsdelivr.net/gh/kellyselden/create-react-app-updater-codemods-manifest@vv1/manifest.json');
-module.exports.getNodeVersion = require('./get-node-version');
-module.exports.getApplicableCodemods = require('./get-applicable-codemods');
 module.exports.resolve = denodeify(require('resolve'));
 module.exports.require = require;
 module.exports.which = denodeify(require('which'));
