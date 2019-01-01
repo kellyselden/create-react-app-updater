6d4f2074ee48d17b290441952fb31cf9c0c0f278
Kelly Selden <kellyselden@gmail.com>
2019-01-01 16:03:49 +0000

    bind getCodemods

--- src/get-applicable-codemods.js
+++ src/get-applicable-codemods.js
@@ -3,14 +3,12 @@
 const utils = require('./utils');
 const semver = require('semver');
 
-const url = 'https://cdn.jsdelivr.net/gh/kellyselden/create-react-app-updater-codemods-manifest@vv1/manifest.json';
-
 module.exports = function getApplicableCodemods({
   startVersion
 }) {
   let nodeVersion = utils.getNodeVersion();
 
-  return utils.getCodemods(url).then(codemods => {
+  return utils.getCodemods().then(codemods => {
     return Object.keys(codemods).filter(codemod => {
       codemod = codemods[codemod];
       let isVersionInRange = semver.gte(startVersion, codemod.version);
