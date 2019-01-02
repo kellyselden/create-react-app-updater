bd542f49a8e7b589f4a0bee1aba9ed035c769fca
Kelly Selden <kellyselden@gmail.com>
2019-01-02 14:52:57 +0000

    move getStartAndEndCommands

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,4 @@
 'use strict';
 
-const denodeify = require('denodeify');
-
 module.exports.run = require('./run');
 module.exports.npx = require('./npx');
-module.exports.resolve = denodeify(require('resolve'));
-module.exports.require = require;
-module.exports.which = denodeify(require('which'));
