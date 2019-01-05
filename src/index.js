60b8fdffdd1bb6b8c4479c568beba4bb9cff8b9e
Kelly Selden <kellyselden@gmail.com>
2019-01-05 14:01:43 +0000

    boilerplate-update refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const fs = require('fs');
-const getPackageJson = require('./get-package-json');
+const getPackageJson = require('boilerplate-update/src/get-package-json');
 const getProjectType = require('./get-project-type');
 const getPackageVersions = require('./get-package-versions');
-const getVersions = require('./get-versions');
+const getVersions = require('boilerplate-update/src/get-versions');
 const getProjectVersion = require('./get-project-version');
 const _getTagVersion = require('./get-tag-version');
 const formatStats = require('./format-stats');
@@ -17,18 +17,7 @@ const run = require('./run-async');
 const getStartAndEndCommands = require('./get-start-and-end-commands');
 const semver = require('semver');
 const co = require('co');
-
-function npm(pkg, field) {
-  return run(`npm info ${pkg} ${field} --json`).then(JSON.parse);
-}
-
-function getTimes(packageName) {
-  return npm(packageName, 'time').then(time => {
-    delete time['created'];
-    delete time['modified'];
-    return time;
-  });
-}
+const getTimes = require('boilerplate-update/src/get-times');
 
 function getVersionAtTime(times, time, margin = 0) {
   time = new Date(new Date(time).getTime() + margin);
@@ -49,14 +38,14 @@ module.exports = function createReactAppUpdater({
   statsOnly,
   listCodemods
 }) {
-  return Promise.resolve().then(() => {
+  return Promise.resolve().then(co.wrap(function*() {
     if (listCodemods) {
       return getCodemods(codemodsUrl).then(codemods => {
         return JSON.stringify(codemods, null, 2);
       });
     }
 
-    let packageJson = getPackageJson('.');
+    let packageJson = yield getPackageJson('.');
     let projectType = getProjectType(packageJson);
     // let versions = getVersions();
     return Promise.all([
@@ -156,5 +145,5 @@ module.exports = function createReactAppUpdater({
         });
       }));
     });
-  });
+  }));
 };
