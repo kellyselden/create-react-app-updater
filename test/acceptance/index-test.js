719e31f8a79ffa2888700bbf3681ae971d545ada
Kelly Selden <kellyselden@gmail.com>
2019-01-22 16:56:34 +0000

    turn on linting

--- test/acceptance/index-test.js
+++ test/acceptance/index-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fs = require('fs-extra');
-const path = require('path');
+// const fs = require('fs-extra');
+// const path = require('path');
 const { expect } = require('chai');
 const {
   processBin,
