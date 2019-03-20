aac8a259e3d809644b6f45807cdd8b362b14393e
Kelly Selden <kellyselden@gmail.com>
2019-03-20 10:56:30 -0700

    add get-start-and-end-commands tests

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,16 @@
 'use strict';
 
-module.exports.run = require('./run');
+module.exports.eject = require('./eject');
+
+module.exports.npxSync = function npxSync(command, ...args) {
+  return require('./run')(`npx ${command}`, ...args);
+};
+
+module.exports.spawn = function spawn() {
+  let ps = require('child_process').spawn(...arguments);
+
+  return new Promise((resolve, reject) => {
+    ps.on('error', reject);
+    ps.on('exit', resolve);
+  });
+};
