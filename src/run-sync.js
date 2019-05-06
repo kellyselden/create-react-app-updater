b045ce25a1e6742be4745c3f82ddadaa5ab9ac40
Kelly Selden <kellyselden@gmail.com>
2019-05-06 09:26:37 +0100

    rename runSync

--- src/run-sync.js
+++ src/run-sync.js
@@ -3,7 +3,7 @@
 const cp = require('child_process');
 const debug = require('debug')('create-react-app-updater');
 
-module.exports = function run(command, options) {
+module.exports = function runSync(command, options) {
   debug(command);
   let result = cp.execSync(command, options).toString();
   debug(result);
