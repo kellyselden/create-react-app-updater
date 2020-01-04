e90cf4d07498ac12024d24ae6a7af8e24d2748df
Kelly Selden <kellyselden@gmail.com>
2020-01-04 17:27:49 +0000

    boilerplate-update@0.31.5

--- test/fixtures/ejected/merge/my-app/config/webpack.config.prod.js
+++ test/fixtures/ejected/merge/my-app/config/webpack.config.prod.js
@@ -260,7 +260,7 @@ module.exports = {
             options: {
               formatter: require.resolve('react-dev-utils/eslintFormatter'),
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -293,7 +293,7 @@ module.exports = {
               customize: require.resolve(
                 'babel-preset-react-app/webpack-overrides'
               ),
-              
+
               plugins: [
                 [
                   require.resolve('babel-plugin-named-asset-import'),
@@ -331,7 +331,7 @@ module.exports = {
               cacheDirectory: true,
               // Save disk space when time isn't as important
               cacheCompression: true,
-              
+
               // If an error happens in a package, it's possible to be
               // because it was compiled. Thus, we don't want the browser
               // debugger to show the original code. Instead, the code
