eb293750ccf5a56724fc9825a69e0600849157c3
Kelly Selden <kellyselden@gmail.com>
2022-05-27 14:46:36 -0700

    use `npm.json`

--- src/get-package-versions.js
+++ src/get-package-versions.js
@@ -31,10 +31,10 @@ async function crawl({
     }
 
     await pRetry(async() => {
-      let results;
+      let dependencies;
 
       try {
-        results = await npm('view', `${parentPackageName}@${_parentVersion}`, 'dependencies', '--json');
+        dependencies = await npm.json('view', `${parentPackageName}@${_parentVersion}`, 'dependencies');
       } catch (err) {
         // occurs sometimes when running multiple npm calls at once
         if (typeof err !== 'string' || !err.includes('npm update check failed')) {
@@ -51,11 +51,10 @@ async function crawl({
       }
 
       // some versions may be missing deps
-      if (!results) {
+      if (!dependencies) {
         return;
       }
 
-      let dependencies = JSON.parse(results);
       let _childVersion = dependencies[childPackageName];
 
       if (_childVersion === childVersion) {
