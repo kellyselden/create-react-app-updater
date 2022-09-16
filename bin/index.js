1b46480c5c3b8cd2bcfc067bbb21f6bf731b7282
Kelly Selden <kellyselden@gmail.com>
2022-09-15 17:25:07 -0700

    remove update-notifier
    to reduce maintenance

--- bin/index.js
+++ bin/index.js
@@ -7,21 +7,6 @@ const args = require('../src/args');
 const { argv } = require('yargs')
   .options(args);
 
-// Displays a message on the terminal if a new version of the package is available.
-const updateNotifier = require('update-notifier');
-const pkg = require('../package.json');
-updateNotifier({
-  pkg,
-  updateCheckInterval: 1000 * 60 * 60 * 24 * 14, // 2 weeks
-  boxenOpts: {
-    padding: 1,
-    margin: 1,
-    align: 'center',
-    borderColor: 'yellow',
-    borderStyle: 'round'
-  }
-}).notify();
-
 (async() => {
   try {
     let message = await createReactAppUpdater(argv);
