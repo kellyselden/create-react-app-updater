b30131c1b6041562e0130156325c36beb3f3ae31
Kelly Selden <kellyselden@gmail.com>
2023-11-05 19:31:56 -0800

    call npx through node
    otherwise `npm ERR! cb.apply is not a function`

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const path = require('path');
+
 module.exports.eject = require('./eject');
 
 module.exports.npxSync = function npxSync(args, options) {
-  return require('./run-sync')('npx', args, {
-    preferLocal: true,
-    localDir: __dirname,
-    ...options
-  });
+  // npm ERR! cb.apply is not a function
+  // return require('./run-sync')('npx', args, {
+  //   preferLocal: true,
+  //   localDir: __dirname,
+  //   ...options
+  // });
+
+  return require('./run-sync')('node', [path.join(path.dirname(require.resolve('npm')), 'bin/npx-cli.js'), ...args], options);
 };
